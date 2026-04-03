d5fe09291e05ba12ad7302fe73de7ba843535262
dylowbyt <andipebrianto12@gmail.com>
2026-04-04 01:47:38 +0700

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -1,53 +1,56 @@
+const axios = require("axios")
 const yts = require("yt-search")
-const ytdl = require("ytdl-core")
 
 module.exports = {
   name: "play",
-  run: async (sock, m) => {
+
+  async run(sock, m) {
+    const from = m.key.remoteJid
+
+    const text =
+      m.message.conversation ||
+      m.message.extendedTextMessage?.text
+
+    const query = text.replace(".play", "").trim()
+
+    if (!query) {
+      return sock.sendMessage(from, {
+        text: "⚠️ Masukkan judul lagu"
+      })
+    }
+
+    // 🔍 Cari lagu
+    const search = await yts(query)
+    const video = search.videos[0]
+
+    if (!video) {
+      return sock.sendMessage(from, {
+        text: "❌ Lagu tidak ditemukan"
+      })
+    }
+
+    await sock.sendMessage(from, {
+      text: `🎵 Ditemukan:\n${video.title}\n⏳ Downloading...`
+    })
+
     try {
-      const from = m.key.remoteJid
+      // 🔥 API downloader (anti error Railway)
+      const res = await axios.get(
+        `https://api.vevioz.com/api/button/mp3/${video.videoId}`
+      )
 
-      const text =
-        m.message.conversation ||
-        m.message.extendedTextMessage?.text
-
-      const query = text.replace(".play", "").trim()
-
-      if (!query) {
-        return sock.sendMessage(from, {
-          text: "Masukin judul lagu!\nContoh: .play cinta untuk starla"
-        })
-      }
-
-      // 🔍 cari di YouTube
-      const search = await yts(query)
-      const video = search.videos[0]
-
-      if (!video) {
-        return sock.sendMessage(from, {
-          text: "Lagu tidak ditemukan 😢"
-        })
-      }
-
-      const url = video.url
+      const url = res.data.match(/href="(.*?)"/)[1]
 
       await sock.sendMessage(from, {
-        text: `🎵 Ditemukan:\n${video.title}\n⏳ Downloading...`
-      })
-
-      // 🎧 download audio
-      const stream = ytdl(url, {
-        filter: "audioonly"
-      })
-
-      await sock.sendMessage(from, {
-        audio: stream,
-        mimetype: "audio/mp4"
+        audio: { url },
+        mimetype: "audio/mpeg",
+        fileName: video.title + ".mp3"
       })
 
     } catch (err) {
-      console.log(err)
-      await sock.sendMessage(m.key.remoteJid, {
+      console.log("DOWNLOAD ERROR:", err.message)
+
+      await sock.sendMessage(from, {
         text: "❌ Gagal download lagu"
       })
     }
