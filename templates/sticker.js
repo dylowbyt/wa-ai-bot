c3449dea74c98359530dc844975684131e2caa10
dylowbyt <andipebrianto12@gmail.com>
2026-04-03 23:16:36 +0700

    Update sticker.js

--- templates/sticker.js
+++ templates/sticker.js
@@ -2,7 +2,7 @@ module.exports = {
   name: "stiker",
   run: async (sock, m) => {
     await sock.sendMessage(m.key.remoteJid, {
-      text: "🎉 fitur stiker aktif (dummy dulu)"
+      text: "✅ stiker ke-detect (next kita bikin real)"
     })
   }
 }
